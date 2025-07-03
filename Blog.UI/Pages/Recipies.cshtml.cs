bcd1a2a3a347c322121df7646047b32812f7941c
Maria Edström <mariat.edstrom@gmail.com>
2025-07-03 16:47:51 +0200

    pages added, new fonts added, frontend update

---
+++ Blog.UI/Pages/Recipies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.UI.Pages
+{
+    public class RecipiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
