e37356698b2d15033e6f19b1fedcf62ccd7db6d4
Maria Edström <mariat.edstrom@gmail.com>
2025-07-21 20:19:16 +0200

    fullstack update

--- Blog.UI/Pages/DoItYourself.cshtml.cs
+++ Blog.UI/Pages/DoItYourself.cshtml.cs
@@ -1,12 +1,51 @@
+using Blog.Dataccess.Entities.Foods;
+using Blog.Shared.DTOs.DIY;
+using Blog.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace Blog.UI.Pages
 {
     public class DoItYourselfModel : PageModel
     {
-        public void OnGet()
+        private readonly DoItYourselfService _doItYourselfService;
+        private readonly ILogger<DoItYourselfModel> _logger;
+
+        public DoItYourselfModel(DoItYourselfService doItYourselfService, ILogger<DoItYourselfModel> logger)
         {
+            _doItYourselfService = doItYourselfService;
+            _logger = logger;
+        }
+
+        public List<DoItYourselfDto> DIYs { get; set; } = new();
+        public List<string> Categories { get; set; } = new();
+
+        public async void OnGet()
+        {
+            try
+            {
+                DIYs = (await _doItYourselfService.GetAllDIYsAsync()).ToList();
+                foreach (var diy in DIYs)
+                {
+                    foreach (var category in diy.Category)
+                    {
+                        if (Categories.Contains(category))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Categories.Add(category);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to fetch diys");
+                // Handle the error appropriately, e.g., show an error message to the user
+            }
         }
     }
 }
--- Blog.UI/Pages/Photografy.cshtml.cs
+++ Blog.UI/Pages/Photografy.cshtml.cs
@@ -1,3 +1,8 @@
+using Blog.Dataccess.Entities.Foods;
+using Blog.Shared.DTOs.DIY;
+using Blog.Shared.DTOs.Photografy;
+using Blog.Shared.Interfaces.Foods;
+using Blog.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +10,43 @@ namespace Blog.UI.Pages
 {
     public class PhotografyModel : PageModel
     {
-        public void OnGet()
+        private readonly PhotografyService _photografyService;
+        private readonly ILogger<PhotografyService> _logger;
+
+        public PhotografyModel(PhotografyService photografyService, ILogger<PhotografyService> logger)
         {
+            _photografyService = photografyService;
+            _logger = logger;
+        }
+
+        public List<PhotoDto> Photos { get; set; } = new();
+        public List<string> Categories { get; set; } = new();
+
+        public async void OnGet()
+        {
+            try
+            {
+                Photos = (await _photografyService.GetAllPhotografiesAsync()).ToList();
+                foreach (var photo in Photos)
+                {
+                    foreach (var category in photo.Category)
+                    {
+                        if (Categories.Contains(category))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Categories.Add(category);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to fetch photos");
+                // Handle the error appropriately, e.g., show an error message to the user
+            }
         }
     }
 }
